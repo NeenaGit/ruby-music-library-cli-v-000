a49e9e23ed0bc214d0e71e31c46c3329ca3f020c
Neena van de Water <nkuriakose@hotmail.com>
2018-07-13 19:50:09 +0000

    automatically backed up by learn

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -71,7 +71,7 @@ def list_songs_by_genre
  def play_song
    puts "Which song number would you like to play?"
    input = gets.strip.to_i
-   if input.between?(1..Song.all.length)
+   if input.between?(1,Song.all.length)
      song = Song.all.sort{ |a, b| a.name <=> b.name }[input - 1]
       puts "Playing #{song.name} by #{song.artist.name}"
    end
