37f2ebe292f1e555bad16633b366fba8d0603e37
Neena van de Water <nkuriakose@hotmail.com>
2018-07-13 19:59:15 +0000

    automatically backed up by learn

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -9,8 +9,7 @@ attr_accessor :path
   def call
     input = ""
 
-    while input !="exit"
-
+    while input != "exit"
       puts "Welcome to your music library!"
       puts "To list all of your songs, enter 'list songs'."
       puts "To list all of the artists in your library, enter 'list artists'."
@@ -21,7 +20,22 @@ attr_accessor :path
       puts "To quit, type 'exit'."
       puts "What would you like to do?"
 
-    input = gets.chomp
+      input = gets.strip
+
+      case input
+      when "list songs"
+        list_songs
+      when "list artists"
+        list_artists
+      when "list genres"
+        list_genres
+      when "list artist"
+        list_songs_by_artist
+      when "list genre"
+        list_songs_by_genre
+      when "play song"
+        play_song
+      end
     end
   end
 
